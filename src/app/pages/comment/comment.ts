98b97f5fb9308e698f1203b8154bf5193b517140
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-07 22:34:40 +0200

    ...

--- src/app/pages/comment/comment.ts
+++ src/app/pages/comment/comment.ts
@@ -1,6 +1,6 @@
 
 import { emitDistinctChangesOnlyDefaultValue } from "@angular/compiler";
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from "@angular/core";
 import { tidslinje } from "../../models/tidslinje";
 import { tidslinjeCommandWrapper } from "../../models/tidslinjeCommandWrapper";
 import { title } from "../../models/title";
@@ -9,56 +9,14 @@ import { title } from "../../models/title";
   selector: "Comment",
   templateUrl: "comment.html"
 })
-export class commentComponent {
+export class commentComponent implements OnChanges {
 
-  //Get change in start and end of selection of text
-  @Input() selectStart: number | undefined;
-  @Output() selectStartChange: EventEmitter<number> = new EventEmitter<number>();
+  ngOnChanges(changes: SimpleChanges) {
 
-  selectStartChangeFun() {
-    this.selectStartChange.emit(this.selectStart);
+    for (let property in changes) {
+
+    }
   }
 
-
-  @Input() selectEnd: number | undefined
-
-  @Output() selectEndChange: EventEmitter<number> = new EventEmitter<number>();
-  selectEndChangeFun() {
-    this.selectEndChange.emit(this.selectEnd);
-  }
-
-
-  //Send selected text between child components
-  @Input() selectedText: string | undefined
-  @Output() selectedTextChange: EventEmitter<string> = new EventEmitter<string>();
-
-  selectedTextChangeFun() {
-    this.selectedTextChange.emit(this.selectedText);
-  }
-
-
-  //Changes from server conserning comments
-  @Input() commandTidslinjeWrapper: Array<tidslinjeCommandWrapper> = new Array<tidslinjeCommandWrapper>();
-  @Output() commandTidslinjeWrapperChange: EventEmitter<Array<tidslinjeCommandWrapper>> = new EventEmitter<Array<tidslinjeCommandWrapper>>();
-
-  commandTidslinjeWrapperFun() {
-    this.commandTidslinjeWrapperChange.emit(this.commandTidslinjeWrapper);
-  }
-
-
-  //When choosen a title, send timelines here
-  @Input() tidslinjerList: Array<tidslinje> = new Array<tidslinje>();
-  @Output() tidslinjerListChange: EventEmitter<Array<tidslinje>> = new EventEmitter<Array<tidslinje>>();
-
-  tidslinjerListChangeFun() {
-    this.tidslinjerListChange.emit(this.tidslinjerList);
-  }
-
-  //When entering website, load all titles.
-  @Input() titleList: Array<title> = new Array<title>();
-  @Output() titleListChange: EventEmitter<Array<title>> = new EventEmitter<Array<title>>();
-
-  titleListChangeFun() {
-    this.titleListChange.emit(this.titleList);
-  }
+ 
 }
--- src/app/pages/comment/components/commentList/commentlist.ts
+++ src/app/pages/comment/components/commentList/commentlist.ts
@@ -1,10 +1,69 @@
 
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from "@angular/core";
+import { tidslinje } from "../../../../models/tidslinje";
+import { tidslinjeCommandWrapper } from "../../../../models/tidslinjeCommandWrapper";
+import { title } from "../../../../models/title";
 
 @Component({
   selector: "commentlist",
   templateUrl: "commentlist.html"
 })
-export class commentlistComponent{
+export class commentlistComponent implements OnChanges {
 
+  ngOnChanges(changes: SimpleChanges) {
+
+    for (let property in changes) {
+
+    }
+  }
+  //Get change in start and end of selection of text
+  @Input('selectStart') selectStart: number | undefined;
+  @Output() selectStartChange: EventEmitter<number> = new EventEmitter<number>();
+
+  selectStartChangeFun() {
+    this.selectStartChange.emit(this.selectStart);
+  }
+
+
+  @Input('selectEnd') selectEnd: number | undefined
+
+  @Output() selectEndChange: EventEmitter<number> = new EventEmitter<number>();
+  selectEndChangeFun() {
+    this.selectEndChange.emit(this.selectEnd);
+  }
+
+
+  //Send selected text between child components
+  @Input('selectedText') selectedText: string | undefined
+  @Output() selectedTextChange: EventEmitter<string> = new EventEmitter<string>();
+
+  selectedTextChangeFun() {
+    this.selectedTextChange.emit(this.selectedText);
+  }
+
+
+  //Changes from server conserning comments
+  @Input('commandTidslinjeWrapper') commandTidslinjeWrapper: Array<tidslinjeCommandWrapper> = new Array<tidslinjeCommandWrapper>();
+  @Output() commandTidslinjeWrapperChange: EventEmitter<Array<tidslinjeCommandWrapper>> = new EventEmitter<Array<tidslinjeCommandWrapper>>();
+
+  commandTidslinjeWrapperFun() {
+    this.commandTidslinjeWrapperChange.emit(this.commandTidslinjeWrapper);
+  }
+
+
+  //When choosen a title, send timelines here
+  @Input('tidslinjerList') tidslinjerList: Array<tidslinje> = new Array<tidslinje>();
+  @Output() tidslinjerListChange: EventEmitter<Array<tidslinje>> = new EventEmitter<Array<tidslinje>>();
+
+  tidslinjerListChangeFun() {
+    this.tidslinjerListChange.emit(this.tidslinjerList);
+  }
+
+  //When entering website, load all titles.
+  @Input('titleList') titleList: Array<title> = new Array<title>();
+  @Output() titleListChange: EventEmitter<Array<title>> = new EventEmitter<Array<title>>();
+
+  titleListChangeFun() {
+    this.titleListChange.emit(this.titleList);
+  }
 }
