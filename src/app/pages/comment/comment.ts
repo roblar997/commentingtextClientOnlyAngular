cce8b3ca4095b0233c222eb26cd47becea35d045
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-07 23:04:14 +0200

    tt

--- src/app/pages/comment/comment.ts
+++ src/app/pages/comment/comment.ts
@@ -17,5 +17,83 @@ export class commentComponent {
   commandTidslinjeWrapper: Array<tidslinjeCommandWrapper> | undefined
   tidslinjerList: Array<tidslinje> | undefined
   titleList: Array<title> | undefined;
+
+  selectStartChangeChild1() {
+
+  }
+  selectStartChangeChild2() {
+
+  }
+  selectStartChangeChild3() {
+
+  }
+  selectStartChangeChild4() {
+
+  }
+
+  selectEndChange1() {
+
+  }
+  selectEndChange2() {
+
+  }
+  selectEndChange3() {
+
+  }
+  selectEndChange4() {
+
+  }
+
+  selectedTextChange1() {
+
+  }
+  selectedTextChange2() {
+
+  }
+  selectedTextChange3() {
+
+  }
+  selectedTextChange4() {
+
+  }
+
+  commandTidslinjeWrapperChange1(commandTidslinjeWrapper: Array<tidslinjeCommandWrapper>) {
+
+  }
+  commandTidslinjeWrapperChange2(commandTidslinjeWrapper: Array<tidslinjeCommandWrapper>) {
+
+  }
+  commandTidslinjeWrapperChange3(commandTidslinjeWrapper: Array<tidslinjeCommandWrapper>) {
+
+  }
+  commandTidslinjeWrapperChange4(commandTidslinjeWrapper : Array<tidslinjeCommandWrapper>) {
+
+  }
+
+  tidslinjerListChange1(tidslinjerList: Array<tidslinje>) {
+
+  }
+  tidslinjerListChange2(tidslinjerList: Array<tidslinje>) {
+
+  }
+  tidslinjerListChange3(tidslinjerList: Array<tidslinje>) {
+
+  } 
+  tidslinjerListChange4(tidslinjerList :Array<tidslinje>) {
+
+  }
+
+titleListChange1(titleList: Array<title>) {
+
+  }
+titleListChange2(titleList: Array<title>) {
+
+  }
+titleListChange3(titleList: Array<title>) {
+
+  }
+titleListChange4(titleList: Array<title>) {
+
+  }
  
 }
