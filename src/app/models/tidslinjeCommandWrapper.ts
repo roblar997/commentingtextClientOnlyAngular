c530d577e76c220916a66cf95f18a8e68b3bc8a1
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-07 22:14:05 +0200

    @input and @output emitters in comment.ts to coordinate messages between child components

--- src/app/models/tidslinjeCommandWrapper.ts
+++ src/app/models/tidslinjeCommandWrapper.ts
@@ -1,1 +1,5 @@
-console.log("Hello World!")
+export class tidslinjeCommandWrapper {
+
+
+
+}
--- src/app/pages/comment/comment.ts
+++ src/app/pages/comment/comment.ts
@@ -1,5 +1,9 @@
 
-import { Component, OnInit } from "@angular/core";
+import { emitDistinctChangesOnlyDefaultValue } from "@angular/compiler";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { tidslinje } from "../../models/tidslinje";
+import { tidslinjeCommandWrapper } from "../../models/tidslinjeCommandWrapper";
+import { title } from "../../models/title";
 
 @Component({
   selector: "Comment",
@@ -7,4 +11,54 @@ import { Component, OnInit } from "@angular/core";
 })
 export class commentComponent {
 
+  //Get change in start and end of selection of text
+  @Input() selectStart: number | undefined;
+  @Output() selectStartChange: EventEmitter<number> = new EventEmitter<number>();
+
+  selectStartChangeFun() {
+    this.selectStartChange.emit(this.selectStart);
+  }
+
+
+  @Input() selectEnd: number | undefined
+
+  @Output() selectEndChange: EventEmitter<number> = new EventEmitter<number>();
+  selectEndChangeFun() {
+    this.selectEndChange.emit(this.selectEnd);
+  }
+
+
+  //Send selected text between child components
+  @Input() selectedText: string | undefined
+  @Output() selectedTextChange: EventEmitter<string> = new EventEmitter<string>();
+
+  selectedTextChangeFun() {
+    this.selectedTextChange.emit(this.selectedText);
+  }
+
+
+  //Changes from server conserning comments
+  @Input() commandTidslinjeWrapper: Array<tidslinjeCommandWrapper> = new Array<tidslinjeCommandWrapper>();
+  @Output() commandTidslinjeWrapperChange: EventEmitter<Array<tidslinjeCommandWrapper>> = new EventEmitter<Array<tidslinjeCommandWrapper>>();
+
+  commandTidslinjeWrapperFun() {
+    this.commandTidslinjeWrapperChange.emit(this.commandTidslinjeWrapper);
+  }
+
+
+  //When choosen a title, send timelines here
+  @Input() tidslinjerList: Array<tidslinje> = new Array<tidslinje>();
+  @Output() tidslinjerListChange: EventEmitter<Array<tidslinje>> = new EventEmitter<Array<tidslinje>>();
+
+  tidslinjerListChangeFun() {
+    this.tidslinjerListChange.emit(this.tidslinjerList);
+  }
+
+  //When entering website, load all titles.
+  @Input() titleList: Array<title> = new Array<title>();
+  @Output() titleListChange: EventEmitter<Array<title>> = new EventEmitter<Array<title>>();
+
+  titleListChangeFun() {
+    this.titleListChange.emit(this.titleList);
+  }
 }
