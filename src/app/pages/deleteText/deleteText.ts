877ad87488672820c9a47c707ab6f449d6c71d90
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-10 16:01:30 +0200

    ttyy

--- src/app/pages/deleteText/deleteText.ts
+++ src/app/pages/deleteText/deleteText.ts
@@ -1,10 +1,17 @@
 
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
   selector: "DeleteText",
   templateUrl: "deleteText.html"
 })
 export class deleteTextComponent {
+  //When entering website, load all titles.
 
+  @Input('titleList') titleList: Array<String> = new Array<String>();
+  @Output() titleListChange: EventEmitter<Array<String>> = new EventEmitter<Array<String>>();
+
+  titleListChangeFun() {
+    this.titleListChange.emit(this.titleList);
+  }//
 }
