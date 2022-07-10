c2830d62b745dfccb7ee243ee53f2b14432d7087
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-07-10 16:02:41 +0200

    bruke annen teknikk, pga router-outlet for å sende info...

--- src/app/pages/deleteText/deleteText.ts
+++ src/app/pages/deleteText/deleteText.ts
@@ -8,10 +8,5 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 export class deleteTextComponent {
   //When entering website, load all titles.
 
-  @Input('titleList') titleList: Array<String> = new Array<String>();
-  @Output() titleListChange: EventEmitter<Array<String>> = new EventEmitter<Array<String>>();
 
-  titleListChangeFun() {
-    this.titleListChange.emit(this.titleList);
-  }//
 }
